62dd5ce5b3ffa3d36db631d7b0dae83367a98ec1
Max Mallett <maxdmallett@gmail.com>
2022-11-16 19:26:01 +0000

    Testimonial carousel data driven

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,4 +1,5 @@
 import Slider from 'react-slick';
+import { testimonalList } from '../data/testimonials';
 
 interface SliderSettings {
     slidesToShow?: number;
@@ -41,36 +42,20 @@ const Testimonials = () => {
             <div className="container grid-xl ">
                 <h2>Testimonials</h2>
                 <Slider {...settings}>
-                    <div>
-                        <p>
-                            &quot;Dionne and her team provide outstanding all-around care. Every aspect is covered to suit our needs and the team are passionate about the persons well-being. I would not hesitate to recommend this caring, lovely team.&quot;
-                        </p>
-                        <p className="quote-ref">Pat, friend of client</p>
-                    </div>
-                    <div>
-                        <p>
-                            &quot;We have been very impressed with the reliable, professional and friendly support provided by Dionne and her staff over this past year. Dee Dee Care really does go above and beyond to care for my mother-in-law and we feel very confident depending on them.&quot;
-                        </p>
-                        <p className="quote-ref">Holly, daughter of client</p>
-                    </div>
-                    <div>
-                        <p>
-                            &quot;I would be happy to recommend Dionne. She always presents as professional and competent and is very conscientious. When supporting customers I have found Dionne will go above and beyond in order to ensure customers get a good service that is person centred and shows them respect and dignity.&quot;
-                        </p>
-                        <p className="quote-ref">Andre, client</p>
-                    </div>
-                    <div>
-                        <p>
-                            &quot;With my admiration for the way you are setting up your agency and helping so many. With my thanks and beyond words for all your inspiration and what you have done for me during 2020.&quot;
-                        </p>
-                        <p className="quote-ref">Michael, client</p>
-                    </div>
-                    <div>
-                        <p>
-                            &quot;Thank you for all your support this year (2020).&quot;
-                        </p>
-                        <p className="quote-ref">Christina, client</p>
-                    </div>
+                    {
+                       testimonalList.map((testimonial, index) => {
+                            return (
+                                <div key={`testimonial-${index}`}>
+                                    <p>
+                                        &quot;{testimonial.quote}&quot;
+                                    </p>
+                                    <p className="quote-ref">
+                                        {testimonial.clientName}
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
                 </Slider>
             </div>		
         </section>
---
+++ data/testimonials.ts
@@ -0,0 +1,27 @@
+interface ITestimonial {
+    quote: string;
+    clientName: string;
+}
+
+export const testimonalList: ITestimonial[] = [
+    {
+        quote: 'Dionne and her team provide outstanding all-around care. Every aspect is covered to suit our needs and the team are passionate about the persons well-being. I would not hesitate to recommend this caring, lovely team.',
+        clientName: 'Pat, friend of client'
+    },
+    {
+        quote: 'We have been very impressed with the reliable, professional and friendly support provided by Dionne and her staff over this past year. Dee Dee Care really does go above and beyond to care for my mother-in-law and we feel very confident depending on them.',
+        clientName: 'Holly, daughter of client'
+    },
+    {
+        quote: 'I would be happy to recommend Dionne. She always presents as professional and competent and is very conscientious. When supporting customers I have found Dionne will go above and beyond in order to ensure customers get a good service that is person centred and shows them respect and dignity.',
+        clientName: 'Andre, client'
+    },
+    {
+        quote: 'With my admiration for the way you are setting up your agency and helping so many. With my thanks and beyond words for all your inspiration and what you have done for me during 2020.',
+        clientName: 'Michael, client'
+    },
+    {
+        quote: 'Thank you for all your support this year.',
+        clientName: 'Christina, client'
+    },
+];
