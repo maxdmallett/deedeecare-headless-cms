3369366d57f68bb1880a2a6ffe8a1755945fb321
Max Mallett <maxdmallett@gmail.com>
2024-11-03 12:25:44 +0000

    Backup testimonial data updated

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -7,6 +7,22 @@ interface ITestimonial {
 
 export const staticTestimonalList: ITestimonial[] = [
     {
+        quote: 'I am happy to recommend Dee Dee Care. Dionne and her colleagues manage to combine efficient service with a personal approach which is adaptable and sensitive to the client’s needs.',
+        client: 'Tim, Norwich'
+    },
+    {
+        quote: 'My Mother has been a client of Dee Dee Care for over 2 years, and they provide a very reliable and caring service. The care given is wonderful and knowledgeable. My mother is over 90 and looks forward to the visits and enjoys being taken out locally. The carers go the extra mile in thoroughly understanding the needs of the client. I would have no hesitation in giving them a 5 star recommendation.',
+        client: 'Geoff, Wymondham'
+    },
+    {
+        quote: 'As a family, we were struggling to find the help our mum so badly needed and deserved. Since Dee Dee Care have been supporting Mum, she has been able to regain some of her independence. Dionne is a very experienced professional who extends her support to the family as well. Thank you - we are extremely grateful to both Dionne and Anna.',
+        client: 'Wendy, Norwich'
+    },
+    {
+        quote: "Dionne has been fantastic with my Dad. She is friendly, kind and caring and I always know when he is with her, that he is in safe hands. Dionne keeps in regular contact with us as a family too, which is lovely and we appreciate her invaluable support. Dionne's other team members are great too and I'd have no hesitation in recommending Dee Dee Care to anyone else in need of help and assistance.",
+        client: 'Louise, Poringland'
+    },
+    {
         quote: 'Dionne and her team provide outstanding all-around care. Every aspect is covered to suit our needs and the team are passionate about the persons well-being. I would not hesitate to recommend this caring, lovely team.',
         client: 'Pat, friend of client'
     },
@@ -27,3 +43,4 @@ export const staticTestimonalList: ITestimonial[] = [
         client: 'Christina, client'
     },
 ];
+
